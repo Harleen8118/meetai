523c8ebd19a3267109c622b95fa5a7dbb6ffa229
Harleen8118 <harleenbbagga@gmail.com>
2025-11-12 15:02:35 +0530

    Test GPG commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className = "text-4xl font-bold text-green-500">
-      Hello world!
-    </div>
+    <Button>
+      Click me!
+    </Button>
   );
 }
