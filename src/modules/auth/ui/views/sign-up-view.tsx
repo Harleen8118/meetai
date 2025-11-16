77eaa4835c58af0fe154687c35633026a8756fa4
Harleen8118 <harleenbbagga@gmail.com>
2025-11-16 22:06:32 +0530

    Refactor development script, add layout and sign-in/sign-up components

---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Card } from "@/components/ui/card"
+
+export const SignUpView = () => {
+  console.log("Sign up view");
+  
+  return (
+    <Card>
+      Sign Up Page
+    </Card>
+  )
+} 
